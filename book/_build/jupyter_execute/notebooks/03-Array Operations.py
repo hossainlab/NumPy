9a3e07d5b3b8bd187ea234b429a48f42cc7e9eec
Jubayer Hossain <work.jubayer@gmail.com>
2020-09-19 17:08:40 +0600

    toc updated

--- book/_build/jupyter_execute/notebooks/03-Array Operations.py
+++ book/_build/jupyter_execute/notebooks/03-Array Operations.py
@@ -1,6 +1,3 @@
-<h1>Table of Contents<span class="tocSkip"></span></h1>
-<div class="toc"><ul class="toc-item"><li><span><a href="#Copying,-Sorting-and-Reshaping" data-toc-modified-id="Copying,-Sorting-and-Reshaping-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Copying, Sorting and Reshaping</a></span><ul class="toc-item"><li><span><a href="#Copy" data-toc-modified-id="Copy-1.1"><span class="toc-item-num">1.1&nbsp;&nbsp;</span>Copy</a></span></li><li><span><a href="#View" data-toc-modified-id="View-1.2"><span class="toc-item-num">1.2&nbsp;&nbsp;</span>View</a></span></li><li><span><a href="#Sorting" data-toc-modified-id="Sorting-1.3"><span class="toc-item-num">1.3&nbsp;&nbsp;</span>Sorting</a></span></li><li><span><a href="#Flatten:-Flattens-2D-array-to-1D-array" data-toc-modified-id="Flatten:-Flattens-2D-array-to-1D-array-1.4"><span class="toc-item-num">1.4&nbsp;&nbsp;</span>Flatten: Flattens 2D array to 1D array</a></span></li><li><span><a href="#Transpose:-Transposes-array-(rows-become-columns-and-vice-versa)" data-toc-modified-id="Transpose:-Transposes-array-(rows-become-columns-and-vice-versa)-1.5"><span class="toc-item-num">1.5&nbsp;&nbsp;</span>Transpose: Transposes array (rows become columns and vice versa)</a></span></li><li><span><a href="#Reshape:-Reshapes-arr-to-r-rows,-c-columns-without-changing-data" data-toc-modified-id="Reshape:-Reshapes-arr-to-r-rows,-c-columns-without-changing-data-1.6"><span class="toc-item-num">1.6&nbsp;&nbsp;</span>Reshape: Reshapes arr to <code>r</code> rows, <code>c</code> columns without changing data</a></span></li><li><span><a href="#Resize:--Changes-arr-shape-to-rxc-and-fills-new-values-with-0" data-toc-modified-id="Resize:--Changes-arr-shape-to-rxc-and-fills-new-values-with-0-1.7"><span class="toc-item-num">1.7&nbsp;&nbsp;</span>Resize:  Changes arr shape to <code>rxc</code> and fills new values with 0</a></span></li><li><span><a href="#References" data-toc-modified-id="References-1.8"><span class="toc-item-num">1.8&nbsp;&nbsp;</span>References</a></span></li></ul></li></ul></div>
-
 # Copying, Sorting and Reshaping
 
 # import numpy 
@@ -85,12 +82,3 @@ A9
 # Resize 
 A9.resize(3, 2)
 A9
-
-## References
-- https://numpy.org/
-- https://www.edureka.co/blog/python-numpy-tutorial/
-- https://github.com/enthought/Numpy-Tutorial-SciPyConf-2019
-- [Python Machine Learning Cookbook](https://www.amazon.com/Python-Machine-Learning-Cookbook-Prateek/dp/1786464470)
-<hr>
-
-*This notebook was created by [Jubayer Hossain](https://jhossain.me/) | Copyright &copy; 2020, [Jubayer Hossain](https://jhossain.me/)*
